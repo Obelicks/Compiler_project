78fb73fa1279f9d31f3f15023c19a8c598dd9d1e
Obelicks <bloechrr@gmail.com>
2018-05-14 12:30:00 +0200

    this is a bad way to print

--- gen/genEXP.c
+++ gen/genEXP.c
@@ -2,7 +2,7 @@
 #include "../headers/gen.h"
 #include"../headers/tree.h"
 void generate_prologue(FILE* fp){
-  fputs("form: \n.string \"%d\n\" \npush %rbp movq %rsp, rbp push %rbx push %r12 push %r13 push %r14 push %r15", fp);
+  printf("form: \n.string \"%d\n\" \n push %rbp \n movq %rsp, rbp \n push %rbx \n push %r12 \n push %r13 \n push %r14 \n push %r15\n");
 }
 /*void generate_FUNC(FILE* fp,FUNC root);{
 
@@ -16,19 +16,19 @@ void generate_EXP_V(FILE* fp,EXP* e){
   switch (e->kind) {
     case equaltoK:
       generate_EXP_V(fp,e->val.equaltoE.left);
-      fputs("cmp %r13, %r14 jeq .16 mov %r13, 1 jmp .11 mov %r13, 0",fp);
+      printf("cmp %r13, %r14 \n jeq .16 \n mov %r13, 1 \n jmp .11 \n mov %r13, 0\n");
       generate_EXP_H(fp,e->val.equaltoE.right);
       break;
 
     case nequaltoK:
       generate_EXP_V(fp,e->val.nequaltoE.left);
-      fputs("cmp %r13, %r14 jne .16 mov %r13, 1 jmp .11 mov %r13, 0",fp);
+      printf("cmp %r13, %r14 \n jne .16 \n mov %r13, 1 \n jmp .11 \n mov %r13, 0\n");
       generate_EXP_H(fp,e->val.nequaltoE.right);
       break;
 
     case andK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r13, 0 jne .34 cmp %r14, 0 jne .16 mov %r13, 0 jmp .11 mov %r13, 1",fp);
+      printf("cmp %r13, 0 \n jne .34 \n cmp %r14, 0 \n jne .16 \n mov %r13, 0 \n jmp .11 \n mov %r13, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
       /*mov to RX*/
       /*mov to RY*/
@@ -37,31 +37,31 @@ void generate_EXP_V(FILE* fp,EXP* e){
 
     case smallerK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jge .16 mov %r13, 0 jmp .11 mov %r13, 1",fp);
+      printf("cmp %r14, %r13 \n jge .16 \n mov %r13, 0 \n jmp .11 \n mov %r13, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
       break;
 
     case biggerK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jle .16 mov %r13, 0 jmp .11 mov %r13, 1",fp);
+      printf("cmp %r14, %r13 \n jle .16 \n mov %r13, 0 \n jmp .11 \n mov %r13, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
        break;
 
     case smalequalK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jg .16 mov %r13, 0 jmp .11 mov %r13, 1",fp);
+      printf("cmp %r14, %r13 \n jg .16 \n mov %r13, 0 \n jmp .11 \n mov %r13, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
        break;
 
     case bigequalK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jl .16 mov %r13, 0 jmp .11 mov %r13, 1",fp);
+      printf("cmp %r14, %r13 \n jl .16 \n mov %r13, 0 \n jmp .11 \n mov %r13, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
       break;
 
      case moduloK:
        generate_EXP_V(fp,e->val.andE.left);
-       fputs("cmp %r13, %r14 jl .22 sub %r13, %r14 jmp .-42",fp);
+       printf("cmp %r13, %r14 \n jl .22 \n sub %r13, %r14 \n jmp .-42");
        generate_EXP_H(fp,e->val.andE.right);
         /*mov to RX*/
        /*mov to RY*/
@@ -70,7 +70,7 @@ void generate_EXP_V(FILE* fp,EXP* e){
 
      case timesK:
         generate_EXP_V(fp,e->val.andE.left);
-        fputs("mov %AX, %r13 mul %r14 mov %r13, %AX",fp);
+        printf("mov %AX, %r13 \n mul %r14 \n mov %r13, %AX\n");
         generate_EXP_H(fp,e->val.andE.right);
          /*mov to RX*/
          /*mov to RY*/
@@ -79,7 +79,7 @@ void generate_EXP_V(FILE* fp,EXP* e){
 
      case divK:
        generate_EXP_V(fp,e->val.andE.left);
-       fputs("mov %AX, %r13 div %r14 mov %r13, %AX",fp);
+       printf("mov %AX, %r13 \n div %r14 \n mov %r13, %AX\n");
        generate_EXP_H(fp,e->val.andE.right);
        /*mov to RX*/
        /*mov to RY*/
@@ -88,7 +88,7 @@ void generate_EXP_V(FILE* fp,EXP* e){
 
      case plusK:
         generate_EXP_V(fp,e->val.andE.left);
-        fputs("add %r13, %r14",fp);
+        printf("add %r13, %r14\n");
         generate_EXP_H(fp,e->val.andE.right);
          /*mov exp to RX*/
        /*mov exp to RY*/
@@ -97,7 +97,7 @@ void generate_EXP_V(FILE* fp,EXP* e){
 
      case minusK:
        generate_EXP_V(fp,e->val.andE.left);
-       fputs("sub %r13, %r14",fp);
+       printf("sub %r13, %r14\n");
        generate_EXP_H(fp,e->val.andE.right);
        /*mov to RX*/
        /*mov to RY*/
@@ -112,13 +112,13 @@ void generate_EXP_V(FILE* fp,EXP* e){
       if (term->kind == boolK){
         aterm = term->val.booleanT;
       }
-      sprintf(a, "mov %%r13,%d", aterm);
-      fputs(a ,fp);
+      sprintf(a, "mov %%r13,%d\n", aterm);
+      printf(a);
       break;
 
     case orK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r13, 0 je .28 cmp %r14, 0 je .11 mov %r13, 1 jmp .11 mov %r13, 0",fp);
+      printf("cmp %r13, 0 je .28 \n cmp %r14, 0 je .11 \n mov %r13, 1 \n jmp .11 \n mov %r13, 0\n");
       generate_EXP_H(fp,e->val.andE.right);
        /*mov to RX*/
        /*mov to RY*/
@@ -137,19 +137,19 @@ void generate_EXP_H(FILE* fp,EXP* e){
   switch (e->kind) {
     case equaltoK:
       generate_EXP_V(fp,e->val.equaltoE.left);
-      fputs("cmp %r13, %r14 jeq .16 mov %r14, 1 jmp .11 mov %r14, 0",fp);
+      printf("cmp %r13, %r14 \n jeq .16 \n mov %r14, 1 \n jmp .11 \n mov %r14, 0\n");
       generate_EXP_H(fp,e->val.equaltoE.right);
       break;
 
     case nequaltoK:
       generate_EXP_V(fp,e->val.nequaltoE.left);
-      fputs("cmp %r13, %r14 jne .16 mov %r14, 1 jmp .11 mov %r14, 0",fp);
+      printf("cmp %r13, %r14 \njne .16 \nmov %r14, 1 \njmp .11 \nmov %r14, 0\n");
       generate_EXP_H(fp,e->val.nequaltoE.right);
       break;
 
     case andK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r13, 0 jne .34 cmp %r14, 0 jne .16 mov %r14, 0 jmp .11 mov %r14, 1",fp);
+      printf("cmp %r13, 0 \n jne .34 \n cmp %r14, 0 \n jne .16 \n mov %r14, 0 \n jmp .11 \n mov %r14, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
     /*mov to RX*/
     /*mov to RY*/
@@ -158,31 +158,31 @@ void generate_EXP_H(FILE* fp,EXP* e){
 
     case smallerK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jge .16 mov %r14, 0 jmp .11 mov %r14, 1",fp);
+      printf("cmp %r14, %r13 \n jge .16 \n mov %r14, 0 \n jmp .11 \n mov %r14, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
       break;
 
     case biggerK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jle .16 mov %r14, 0 jmp .11 mov %r14, 1",fp);
+      printf("cmp %r14, %r13 \n jle .16 \n mov %r14, 0 \n jmp .11 \n mov %r14, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
       break;
 
     case smalequalK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jg .16 mov %r14, 0 jmp .11 mov %r14, 1",fp);
+      printf("cmp %r14, %r13 \n jg .16 \n mov %r14, 0 \n jmp .11 \n mov %r14, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
       break;
 
     case bigequalK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r14, %r13 jl .16 mov %r14, 0 jmp .11 mov %r14, 1",fp);
+      printf("cmp %r14, %r13 \n jl .16 \n mov %r14, 0 \n jmp .11 \n mov %r14, 1\n");
       generate_EXP_H(fp,e->val.andE.right);
       break;
 
      case moduloK:
        generate_EXP_V(fp,e->val.andE.left);
-       fputs("cmp %r13, %r14 jl .22 sub %r13, %r14 jmp .-42 mov %r14, %r13",fp);
+       printf("cmp %r13, %r14 \n jl .22 \n sub %r13, %r14 \n jmp .-42 \n mov %r14, %r13\n");
        generate_EXP_H(fp,e->val.andE.right);
          /*mov to RX*/
          /*mov to RY*/
@@ -191,7 +191,7 @@ void generate_EXP_H(FILE* fp,EXP* e){
 
      case timesK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("mov %AX, %r14 mul %r13 mov %r14, %AX",fp);
+      printf("mov %AX, %r14 \n mul %r13 \n mov %r14, %AX\n");
       generate_EXP_H(fp,e->val.andE.right);
        /*mov to RX*/
        /*mov to RY*/
@@ -200,7 +200,7 @@ void generate_EXP_H(FILE* fp,EXP* e){
 
     case divK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs(" mov %AX, %r13 div %r14 mov %r14, %AX",fp);
+      printf(" mov %AX, %r13 \n div %r14 \n mov %r14, %AX\n");
       generate_EXP_H(fp,e->val.andE.right);
        /*mov to RX*/
        break;
@@ -209,7 +209,7 @@ void generate_EXP_H(FILE* fp,EXP* e){
 
    case plusK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("add %r14, %r13",fp);
+      printf("add %r14, %r13\n");
       generate_EXP_H(fp,e->val.andE.right);
       /*mov exp to RX*/
       /*mov exp to RY*/
@@ -218,7 +218,7 @@ void generate_EXP_H(FILE* fp,EXP* e){
 
    case minusK:
      generate_EXP_V(fp,e->val.andE.left);
-     fputs("sub %r13, %r14 mov %r14, %r13",fp);
+     printf("sub %r13, %r14 \n mov %r14, %r13\n");
      generate_EXP_H(fp,e->val.andE.right);
      /*mov to RY*/
      /*mov to RX*/
@@ -232,13 +232,13 @@ void generate_EXP_H(FILE* fp,EXP* e){
       if (term->kind == boolK){
         aterm = term->val.booleanT;
       }
-      sprintf(a, "mov %%r14,%d", aterm);
-      fputs(a ,fp);
+      sprintf(a, "mov %%r14,%d \n", aterm);
+      printf(a );
       break;
 
     case orK:
       generate_EXP_V(fp,e->val.andE.left);
-      fputs("cmp %r13, 0 je .28 cmp %r14, 0 je .11 mov %r14, 1 jmp .11 mov %r14, 0",fp);
+      printf("cmp %r13, 0 \n je .28 \n cmp %r14, 0 je .11 \n mov %r14, 1 \n jmp .11 \n mov %r14, 0\n");
       generate_EXP_H(fp,e->val.andE.right);
      /*mov to RX*/
      /*mov to RY*/
@@ -246,11 +246,12 @@ void generate_EXP_H(FILE* fp,EXP* e){
      break;
 
   default:
-     printf("in default case in EXP\n");
+  fprintf( stderr, "default case reached");
+
      break;
   }
 }
 
 void generate_epilogue(FILE* fp){
-  fputs("pop %r15 pop %r14 pop %r13 pop %r12 pop %rbx movq %rbp, %rsppop %rbp mov1 %0, %rax ret",fp);
+  printf("pop %r15 \n pop %r14 \n pop %r13 \n pop %r12 \n pop %rbx \n movq %rbp, %rsp \n pop %rbp \n mov1 %0, %rax \n ret\n");
 }
