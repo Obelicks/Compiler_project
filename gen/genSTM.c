926911d8e9352a81e386eb201db6326b18aa539d
Emil Christian Bløcher <SDU\emblo15@imada-106311.adm.c.sdu.dk>
2018-12-02 15:49:51 +0100

    pff

--- gen/genSTM.c
+++ gen/genSTM.c
@@ -128,6 +128,10 @@ void generate_STM(STM* s){
 
     case stmlistK:
       fprintf(stderr, "generating generate_STM -> stmlistK \n" );
+      //fprintf(stderr,"statelist2\n");
+      if (s->val.stmlistS != NULL){
+        generate_LIST(s->val.stmlistS);
+      }
       break;
 
     default:
